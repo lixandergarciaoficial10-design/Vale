41c4e02c5864b12c0378ab61bea9bf012636e04f
lixandergarciaoficial10-design <lixandergarciaoficial10@gmail.com>
2026-04-17 15:46:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -927,8 +927,13 @@ elif menu == "👥 Todos mis Clientes":
                                         <img src="https://upload.wikimedia.org/wikipedia/commons/a/aa/Google_Maps_icon_%282020%29.svg" width="20">
                                     </button></a>''', unsafe_allow_html=True)
                             else:
-                                st.button("📵", disabled=True, key=f"no_gps_{cl['id']}", use_container_width=True)
-                                help="No se han guardado las coordenadas GPS de este cliente"
+                                # El botón de "No hay GPS" con el mensaje flotante (help)
+                                st.button(
+                                    "📵", 
+                                    disabled=True, 
+                                    key=f"no_gps_{cl['id']}", 
+                                    help="No se han guardado las coordenadas GPS de este cliente",
+                                    use_container_width=True
 
                         # ELIMINAR DISCRETO
                         with st.popover("⚙️", use_container_width=True):
